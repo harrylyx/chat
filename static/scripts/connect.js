30d947c3d66b12c1c17142d9f2ec77a0e2252393
SlaneYang <904509221@qq.com>
2016-10-16 22:47:47 +0800

    修复路由错误问题

--- static/scripts/connect.js
+++ static/scripts/connect.js
@@ -1,13 +1,14 @@
 /**
  * Created by Slane on 2016/10/16.
  */
-var address = 'ws://172.18.61.245:8000/';
+var address = 'ws://172.18.61.245:8000/soc';
 var ws = new WebSocket(address);
 ws.onmessage = function(event) {
     var content;
     var data =  JSON.parse(event.data);
     var container = $('#J_message');
     console.log(data);
+
     if(data.type == 'sys'){
         content = "<p>" + data.message + "</p>";
         container.append(content);
@@ -15,38 +16,23 @@ ws.onmessage = function(event) {
         content = "<p>" + data.message + "</p><p>" + data.name + "</p>" + "<span>" + data.time + "</span>";
         container.append(content);
     }
-    // ({
-    //     'sys': function() {
-    //         var cell = table.insertRow().insertCell();
-    //         cell.colSpan = 2;
-    //         cell.innerHTML = data['message'];
-    //     },
-    //     'user': function() {
-    //         var row = table.insertRow();
-    //         row.insertCell().innerHTML = data['message'];
-    //         row.insertCell().innerHTML = data['id'];
-    //     },
-    // }[data['type']])();
+
 };
 
-// $('#J_send').on('click touch', function() {
-//     var ele = document.getElementById('chat');
-//     if (ele.value) {
-//         ws.send(ele.value);
-//         ele.value = '';
-//     } else {
-//         console.log("nothing");
-//     }
-// });
-// document.onkeydown=function() {
-//     //绑定回车键
-//     var e = event || window.event;
-//     if (e.keyCode == 13) {
-//         $('#J_send').trigger('click');
-//     };
-//
-// };
-function send() {
-    ws.send(document.getElementById('chat').value);
-    document.getElementById('chat').value = '';
-}
+$('#J_send').on('click touch', function() {
+    var ele = document.getElementById('chat');
+    if (ele.value) {
+        ws.send(ele.value);
+        ele.value = '';
+    } else {
+        console.log("nothing");
+    }
+});
+document.onkeydown=function() {
+    //绑定回车键
+    var e = event || window.event;
+    if (e.keyCode == 13) {
+        $('#J_send').trigger('click');
+    };
+
+};
