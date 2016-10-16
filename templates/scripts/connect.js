12c1cded8ddf4b8c6f779b10cb01c6f3e2bbf110
SlaneYang <904509221@qq.com>
2016-10-16 16:47:30 +0800

    修改connect.js

--- templates/scripts/connect.js
+++ templates/scripts/connect.js
@@ -4,20 +4,28 @@
 var address = 'ws://172.18.61.245:8000/soc';
 var ws = new WebSocket(address);
 ws.onmessage = function(event) {
-    var table = document.getElementById('message');
-    var data = eval('(' + event.data + ')');
-    ({
-        'sys': function() {
-            var cell = table.insertRow().insertCell();
-            cell.colSpan = 2;
-            cell.innerHTML = data['message'];
-        },
-        'user': function() {
-            var row = table.insertRow();
-            row.insertCell().innerHTML = data['message'];
-            row.insertCell().innerHTML = data['id'];
-        },
-    }[data['type']])();
+    var content;
+    var data =  JSON.parse(event.data);
+    var container = $('#J_message');
+    if(data.type == 'sys'){
+        content = "<p>" + data.message + "</p>";
+        container.appendTo(content);
+    } else if (data.type == 'user') {
+        content = "<p>" + data.message + "</p><p>" + data.id + "</p>";
+        container.appendTo(content);
+    }
+    // ({
+    //     'sys': function() {
+    //         var cell = table.insertRow().insertCell();
+    //         cell.colSpan = 2;
+    //         cell.innerHTML = data['message'];
+    //     },
+    //     'user': function() {
+    //         var row = table.insertRow();
+    //         row.insertCell().innerHTML = data['message'];
+    //         row.insertCell().innerHTML = data['id'];
+    //     },
+    // }[data['type']])();
 };
 function send() {
     ws.send(document.getElementById('chat').value);
