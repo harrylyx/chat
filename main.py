7a62a6ff980d17c65fbb49ec2c0dbbbf7955b550
harrylyx <lyxvae@gmail.com>
2016-11-21 14:19:42 +0800

    加入黑名单，加入反馈

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class Index(tornado.web.RequestHandler):
 class SocketHandler(tornado.websocket.WebSocketHandler):
     clients = set()
     mdict = dict()
+    online_ip_agent = []
 
     def send_to_all(self,message):
         for c in SocketHandler.clients:
@@ -58,57 +59,76 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
         return SocketHandler.mdict[str(id(self))]
 
     def open(self):
-        cx = MySQLdb.connect("localhost", "root", "lyx15&lyx", "chat")
-        cursor = cx.cursor()
         user_agent = self.request.headers['user-agent'].replace("\'","|")
         ip = self.request.headers.get("X-Real-IP")
-        cursor.execute("insert into online (id,ip,user_agent) values (%d,'%s','%s')"%(id(self),ip,user_agent))
-        cursor.close()
-        cx.commit()
-        cx.close()
-        mname = SocketHandler.get_name(self)
-        SocketHandler.clients.add(self)
-        self.write_message(json.dumps({
-            'type': 'sys',
-            'id':id(self),
-            'person':len(SocketHandler.clients),
-            'message': 'Welcome to WebSocket',
-        }))
-        self.write_message(json.dumps({
-            'type': 'bot',
-            'time':time.strftime("%H:%M:%S", time.localtime()),
-            'id':id(self)+12138,
-            'name': 'Master robot',
-            'messageType':3,
-            'message': '''欢迎你,%s!
-                       <br>我是这里的机器人,管理着这个聊天室
-                       <br>如果你是第一次来到这里，可以发送\\help获取使用帮助
-                       <br>不要乱搞哦，我会看着你的.'''%(mname),
-        }))
-        SocketHandler.send_to_all(self,{
-            'type': 'sys',
-            'person':len(SocketHandler.clients),
-            'message': mname + ' has joined',
-        })
-
+        cx = MySQLdb.connect("localhost", "root", "lyx15&lyx", "chat")
+        cursor = cx.cursor()
+        cursor.execute('select ip from blacklist')
+        values = cursor.fetchall()  # 获取查询的值
+        ip = tuple("'"+str(ip)+"'"+',')
+        if ip in values:
+            self.write_message(json.dumps({
+                'type': 'bot',
+                'time':time.strftime("%H:%M:%S", time.localtime()),
+                'id':id(self)+12138,
+                'name': 'Master robot',
+                'messageType':3,
+                'message': '''欢迎你成功进入黑名单!
+                           <br>喊你乱搞，你已经被禁止进入了,哼!''',
+            }))
+            cursor.close()
+            cx.commit()
+            cx.close()
+            super(self).on_close()
+        else:
+            cursor.execute("insert into online (id,ip,user_agent,time) values (%d,'%s','%s',%s)"
+                           %(id(self),ip,user_agent,time.strftime("%H:%M:%S", time.localtime())))
+            cursor.close()
+            cx.commit()
+            cx.close()
+            mname = SocketHandler.get_name(self)
+            SocketHandler.clients.add(self)
+            self.write_message(json.dumps({
+                'type': 'sys',
+                'id':id(self),
+                'person':len(SocketHandler.clients),
+                'message': 'Welcome to WebSocket',
+            }))
+            self.write_message(json.dumps({
+                'type': 'bot',
+                'time':time.strftime("%H:%M:%S", time.localtime()),
+                'id':id(self)+12138,
+                'name': 'Master robot',
+                'messageType':3,
+                'message': '''欢迎你,%s!
+                           <br>我是这里的机器人,管理着这个聊天室
+                           <br>如果你是第一次来到这里，可以发送\\help获取使用帮助
+                           <br>不要乱搞哦，我会看着你的.'''%(mname),
+            }))
+            SocketHandler.send_to_all(self,{
+                'type': 'sys',
+                'person':len(SocketHandler.clients),
+                'message': mname + ' has joined',
+            })
 
     def on_close(self):
-        cx = MySQLdb.connect("localhost", "root", "lyx15&lyx", "chat")
-        cursor = cx.cursor()
         user_agent = self.request.headers['user-agent'].replace("\'","|")
         ip = self.request.headers.get("X-Real-IP")
+        cx = MySQLdb.connect("localhost", "root", "lyx15&lyx", "chat")
+        cursor = cx.cursor()
         try:
             cursor.execute("delete from online where id = %d"%(id(self)))
         except:
             pass
-        cursor.execute("insert into offline (id,ip,user_agent) values (%d,'%s','%s')"%(id(self),ip,user_agent))
+        cursor.execute("insert into offline (id,ip,user_agent,time) values (%d,'%s','%s',%s)"
+                       %(id(self),ip,user_agent,time.strftime("%H:%M:%S", time.localtime())))
         cursor.close()
         cx.commit()
         cx.close()
         mname = SocketHandler.get_name(self)
         SocketHandler.clients.remove(self)
         SocketHandler.send_to_all(self,{
-            'type': 'sys',
+           'type': 'sys',
             'person':len(SocketHandler.clients),
             'message': mname + ' has left',
         })
@@ -127,7 +147,7 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
                                                          'markdown.extensions.codehilite']),
             })
         elif re.search('^\$\$.*\$\$$',str(message).encode('utf-8'),re.S):
-
+            message = message.strip()
             message = '<img src="https://l.wordpress.com/latex.php?bg=ffffff&fg=000000&s=0&latex={0}" style="border:none;">'.format(message[2:-2])
             SocketHandler.send_to_all(self,{
                 'type': 'user',
@@ -198,13 +218,46 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
                                 <br>```python<br>print('Hello world!')<br>```
                                 <br>支持使用LaTeX语法发送<strong>公式</strong><br>Example:<br>$$h = \\frac{1}{2}gt^2$$
                                 <br>支持使用<strong>\\weather 地点(拼音)</strong>查看天气
-                                <br>支持使用<strong>\\news查看新闻</strong>''',
+                                <br>支持使用<strong>\\news</strong>查看新闻
+                                <br>如果有什么建议，欢迎使用<strong>\\feedback 内容</strong>给我们反馈''',
+            }))
+        elif re.search('^\\\\feedback',str(message).encode('utf-8'),re.S):
+            cleaner = lxml.html.clean.Cleaner(style=True, scripts=True, frames = True,
+                                              forms = True,page_structure=False, safe_attrs_only=False)
+            message = cleaner.clean_html(message)
+            message = message[10:]
+            user_agent = self.request.headers['user-agent'].replace("\'","|")
+            ip = self.request.headers.get("X-Real-IP")
+            cx = MySQLdb.connect("localhost", "root", "lyx15&lyx", "chat")
+            cursor = cx.cursor()
+            cursor.execute("insert into online (id,ip,user_agent,time,feedback) values (%d,'%s','%s',%s,'%s')"
+                           %(id(self),ip,user_agent,time.strftime("%H:%M:%S", time.localtime()),message))
+            cursor.close()
+            cx.commit()
+            cx.close()
+            self.write_message(json.dumps({
+                'type': 'user',
+                'time':time.strftime("%H:%M:%S", time.localtime()),
+                'id':id(self),
+                'itisme':1,
+                'name': mname,
+                'messageType':3,
+                'message': message,
+            }))
+            self.write_message(json.dumps({
+                'type': 'bot',
+                'time':time.strftime("%H:%M:%S", time.localtime()),
+                'id':id(self)+12138,
+                'name': 'Master robot',
+                'messageType':3,
+                'message': '''我们已经收到你的反馈了，谢谢你!''',
             }))
         elif message == 'c93c60882b37254bb13e80183f291af3':
             pass
         else:
             message = message.replace('\n','<br>')
-            cleaner = lxml.html.clean.Cleaner(style=True, scripts=True, page_structure=False, safe_attrs_only=False)
+            cleaner = lxml.html.clean.Cleaner(style=True, scripts=True, frames = True,
+                                              forms = True,page_structure=False, safe_attrs_only=False)
             message = cleaner.clean_html(message)
             SocketHandler.send_to_all(self,{
                 'type': 'user',
@@ -214,6 +267,31 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
                 'messageType':3,
                 'message': message,
             })
+
+    def check_ip(self):
+        user_agent = self.request.headers['user-agent'].replace("\'","|")
+        ip = self.request.headers.get("X-Real-IP")
+        cx = MySQLdb.connect("localhost", "root", "lyx15&lyx", "chat")
+        cursor = cx.cursor()
+        cursor.execute('select ip from offline')
+        values_ip = cursor.fetchall()  # 获取查询ip
+        cursor.execute('select ip from time')
+        values_time = cursor.fetchall()  # 获取查询time
+        ip = tuple("'"+str(ip)+"'"+',')
+        black = 0
+        for i in range(len(values_ip)-1,len(values_ip)-11,-1):
+            fisrt = time.mktime(time.strptime('2016-01-01 '+values_time[i],'%Y-%m-%d %H:%M:%S'))
+            second = time.mktime(time.strptime('2016-01-01 '+values_time[i-1],'%Y-%m-%d %H:%M:%S'))
+            if ip == values_ip[i] and fisrt - second < 5:
+                black += 1
+        if black == 10:
+            cursor.execute("insert into online (id,ip,user_agent,time) values (%d,'%s','%s',%s)"
+                           %(id(self),ip,user_agent,time.strftime("%H:%M:%S", time.localtime())))
+        cursor.close()
+        cx.commit()
+        cx.close()
+
+
 
 
 if __name__ == '__main__':
